0ff74df140eccd33573c87284d28fb51f24e14fa
박지만 <gman.park@nhn.com>
2014-04-04 10:10:21 +0900

    init

---
+++ src/namespace.js
@@ -0,0 +1,20 @@
+/**
+ *  @author Park ji man
+ *  @version 0.0.1
+ *  @desc NHN technology service : javascript namespace
+ */
+
+var nts = nts || {};
+
+/**
+ * @desc nts Component module
+ */
+
+nts.Component = nts.Component || {};
+
+/**
+ *  @desc nts Drag & Drop Component Namespace
+ * */
+
+nts.Component.DragModel = nts.Component.DragModel || {};
+nts.Component.DropModel = nts.Component.DropModel || {};
